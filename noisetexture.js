4b6d194dd8a23ba7a9f51410952ece2a655ac487
Dan Lecocq <dlecocq@dan-lecocqs-macbook-2.local>
2010-01-11 13:22:11 +0300

    Noisetexture is now a normal object, not a WebGL texture.

--- noisetexture.js
+++ noisetexture.js
@@ -1,25 +1,27 @@
 /* http://learningwebgl.com/blog/?p=507
  */
-function noisetexture(context, scr) {
+function noisetexture(context, width, height) {
 	
 	this.texture = null;
 	this.image	 = null;
 	
 	this.gl			 = context;
-	this.scr     = scr;
+	
+	this.width   = width;
+	this.height  = height;
 
-	this.initialize = function() {		
+	this.initialize = function() {
 		this.texture = this.gl.createTexture();
 		this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
 
-		var pixels = new WebGLFloatArray(scr.width * scr.height * 4);
-		var count = scr.width * scr.height * 4;
+		var pixels = new WebGLFloatArray(this.width * this.height * 4);
+		var count = this.width * this.height * 4;
 		//*
-		for (var i = 0; i < count; i += 1) {
+		for (var i = 0; i < count; i += 4) {
 			pixels[i] = Math.random() * 3.0;
 		}
 		//*/
-		this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, scr.width, scr.height, 0, this.gl.RGBA, this.gl.FLOAT, pixels);
+		this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.width, this.height, 0, this.gl.RGBA, this.gl.FLOAT, pixels);
 		
 		this.gl.enable(this.gl.TEXTURE_2D);
 		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
@@ -36,5 +38,5 @@ function noisetexture(context, scr) {
 	
 	this.initialize();
 	
-	return this.texture;
+	//return this.texture;
 }
