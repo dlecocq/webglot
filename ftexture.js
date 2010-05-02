82b2b6c805a3647568a417024ee35edcc7886e80
Dan Lecocq <dan.lecocq@kaust.edu.sa>
2010-05-02 16:34:47 +0300

    Added support for a texture that accepts a function that defines the texture.  This will drastically cut down on the number of texture classes I will need.

---
+++ ftexture.js
@@ -0,0 +1,43 @@
+/* http://learningwebgl.com/blog/?p=507
+ */
+function ftexture(context, width, height, f) {
+	
+	this.texture = null;
+	this.image	 = null;
+	
+	this.gl			 = context;
+	
+	this.width   = width;
+	this.height  = height;
+
+	this.initialize = function(f) {
+		this.texture = this.gl.createTexture();
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+
+		var pixels = new WebGLFloatArray(this.width * this.height * 4);
+		pixels = f(pixels);
+		/*
+		var count = this.width * this.height * 4;
+		for (var i = 0; i < count; i += 4) {
+			pixels[i] = Math.random() * 3.0;
+		}
+		//*/
+		this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.width, this.height, 0, this.gl.RGBA, this.gl.FLOAT, pixels);
+		
+		this.gl.enable(this.gl.TEXTURE_2D);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_S_WRAP, this.gl.CLAMP);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_T_WRAP, this.gl.CLAMP);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+	}
+	
+	this.bind = function() {
+		this.gl.enable(this.gl.TEXTURE_2D);
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+	}
+	
+	this.initialize(f);
+	
+	return this.texture;
+}
