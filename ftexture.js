e17567a0cb1005a08fa66726c2df90e0424bb78c
Dan Lecocq <dan.lecocq@kaust.edu.sa>
2010-05-11 16:05:51 +0300

    Added a mechnism for the programmer to specify the format he'd like for on-card storage.  It defaults to floating-point.

--- ftexture.js
+++ ftexture.js
@@ -1,9 +1,11 @@
 /* http://learningwebgl.com/blog/?p=507
  */
-function ftexture(context, width, height, f) {
+function ftexture(context, width, height, f, type) {
 	
 	this.texture = null;
 	this.image	 = null;
+	
+	this.type    = type || 0x8814;
 	
 	this.gl			 = context;
 	
@@ -17,7 +19,7 @@ function ftexture(context, width, height, f) {
 		var pixels = new WebGLFloatArray(this.width * this.height * 4);
 		// Pass pixels into the user-provided function
 		pixels = f(pixels);
-		this.gl.texImage2D(this.gl.TEXTURE_2D, 0, 0x8814, this.width, this.height, 0, this.gl.RGBA, this.gl.FLOAT, pixels);
+		this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.type, this.width, this.height, 0, this.gl.RGBA, this.gl.FLOAT, pixels);
 		
 		this.gl.enable(this.gl.TEXTURE_2D);
 		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
